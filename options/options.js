807ed1a26e1a2d414621ce59376de36dfc39df72
Crowdbotics <team@crowdbotics.com>
2024-02-05 04:17:28 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "inspire-dinerewards-45855",
+  url: "https://inspire-dinerewards-45855.botics.co",
+  api: "https://inspire-dinerewards-45855.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
